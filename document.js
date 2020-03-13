1aff91a0af639ed0758d45de26e7408c05961c8c
Misha Reyzlin <mrejzlin@gmail.com>
2020-03-13 01:25:11 +0100

    Adds favicon

--- document.js
+++ document.js
@@ -5,6 +5,11 @@ export default function(file, body) {
   <title>Free Time Managers</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
+
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+
   <link href="/main.css" rel="stylesheet" />
   <link rel="stylesheet" href="/leaflet.css" />
 <!--  <link rel="stylesheet"-->
