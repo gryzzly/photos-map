157efb438bbf57159e43af8749d7e54af81a1dc8
Misha Reyzlin <mrejzlin@gmail.com>
2020-04-22 02:02:59 +0200

    preload links should have absolute links

--- document.js
+++ document.js
@@ -10,8 +10,8 @@ export default function(file, body) {
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 
-  <link rel="preload" href="components/App.js" as="script">
-  <link rel="preload" href="web_modules/preact.js" as="script">
+  <link rel="preload" href="/components/App.js" as="script">
+  <link rel="preload" href="/web_modules/preact.js" as="script">
 
   <link href="/main.css" rel="stylesheet" />
   <link rel="stylesheet" href="/leaflet.css" />
